bc35eb744b3fed1daf5f2642b9ec49e1eb8a5f31
vanita eckert <vanita112003@gmail.com>
2017-01-14 03:32:24 +0000

    new file added

--- lib/country.rb
+++ lib/country.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
-class Country
+class LeastTravelledCountries::Country
 
-  attr_accessor :name, :info
+  attr_accessor :name, :info, :doc
 
   @@all = []
 
@@ -17,9 +17,10 @@ class Country
       )
   end
 
-  def initialize(name=nil, information=nil)
+  def initialize(name=nil, information=nil, doc=nil)
     @name = name
     @info = info
+    @doc = doc
     @@all << self
     # when an instance of country is created every one of this instances will have a name & info
     # and all these attributes will be pushed into @@all the class variable.
@@ -34,33 +35,33 @@ class Country
     self.all[id-1]
   end
 
-  # def best_dish
-  #   @best_dish ||= doc.xpath("//div[@class='c-4 nr nt']/ul[3]/li").text
-  # end
-  #
-  # def food_style
-  #   @food_style ||= doc.xpath("//div[@class='c-4 nr nt']/ul[2]/li").text
-  # end
-  #
-  # def contact
-  #   @contact ||= doc.xpath("//div[@class='c-4 nr nt']/ul[4]/li[1]").text.split("+").join(". Tel: +")
-  # end
-  #
-  # def head_chef
-  #   @head_chef ||= doc.xpath("//div[@class='c-4 nr nt']/ul[1]/li").text.split(" (pictured)").join("")
-  # end
-  #
-  # def website_url
-  #   @website_url ||= doc.xpath("//div[@class='c-4 nr nt']/ul[4]/li[2]/a").text
-  # end
-  #
-  # def description
-  #   @description ||= doc.xpath("//div[@class='c-8 nl nt']/p[3]").text
-  # end
-  #
-  # def doc
-  #   @doc ||= Nokogiri::HTML(open(self.url))
-  # end
+  def best_dish
+    @best_dish ||= doc.xpath("//div[@class='c-4 nr nt']/ul[3]/li").text
+  end
+
+  def food_style
+    @food_style ||= doc.xpath("//div[@class='c-4 nr nt']/ul[2]/li").text
+  end
+
+  def contact
+    @contact ||= doc.xpath("//div[@class='c-4 nr nt']/ul[4]/li[1]").text.split("+").join(". Tel: +")
+  end
+
+  def head_chef
+    @head_chef ||= doc.xpath("//div[@class='c-4 nr nt']/ul[1]/li").text.split(" (pictured)").join("")
+  end
+
+  def website_url
+    @website_url ||= doc.xpath("//div[@class='c-4 nr nt']/ul[4]/li[2]/a").text
+  end
+
+  def description
+    @description ||= doc.xpath("//div[@class='c-8 nl nt']/p[3]").text
+  end
+
+  def doc
+    @doc ||= Nokogiri::HTML(open(self.url))
+  end
 end
 
 
