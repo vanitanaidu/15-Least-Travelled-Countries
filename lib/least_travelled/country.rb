9ecbe2d6ebb8f03e35a2effe956dce6c99b83b53
vanita eckert <vanita112003@gmail.com>
2017-01-19 02:42:16 +0000

    add new code

--- lib/least_travelled/country.rb
+++ lib/least_travelled/country.rb
@@ -2,23 +2,15 @@ require_relative '../least_travelled'
 
 class Country
 
-  attr_accessor :name, :url, :no_of_tourists, :information, :doc
+  attr_accessor :name, :tourists, :info
 
   @@all = []
 
-  def self.new_from_main_page(c)
-     self.new(
-       c.css("h3.slide-title").text,
-       "http://www.thisisinsider.com/the-least-visited-countries-in-the-world-2015-7"
-      )
-        # #{c.css(".btn.resize href").text}
-        # "http://www.thisisinsider.com/the-least-visited-countries-in-the-world-2015-7#{c.css("a").attribute("href").text}"
-         #name alone no numbers no tourists
-  end
 
-  def initialize
+  def initialize(name=nil, tourists=nil, info=nil)
     @name = name
-    @url =  url
+    @tourists = tourists
+    @info = info
     @@all << self
     # when an instance of country is created every one of the instances will have a name
     # and it will be pushed into @@all the class variable.
@@ -32,24 +24,5 @@ class Country
     self.all[id-1]
   end
 
-  def no_of_tourists
-    binding.pry
-doc.css("")
-
-  end
-
-  def information
-  end
-
-  def doc
-
-    @doc = Nokogiri::HTML(open(self.url))
-    binding.pry
-
-  end
 
 end
-
-country = Country.new
-country.doc
-# scraper = Scraper.new
--- lib/least_travelled/scraper.rb
+++ lib/least_travelled/scraper.rb
@@ -2,24 +2,46 @@ require_relative '../least_travelled'
 
 class Scraper
 
-  def get_page
+  def self.get_page
     Nokogiri::HTML(open("http://www.thisisinsider.com/the-least-visited-countries-in-the-world-2015-7"))
+
   end
 
-  def scrape_main_page
+  def self.scrape_main_page
     sleep 1
-    self.get_page.css("div.slide-module")
+    get_page.css("div.slide-module")
+   end
+
+   def self.scrape_country_info
+
+     info = scrape_main_page.css("p span")
+     info.each do |each_info|
+       each_info.text
+     end
    end
 
 
-   def create_countries
-    self.scrape_main_page.each do |c|
-      Country.new_from_main_page(c)
+
+   def self.create_countries
+    scrape_main_page.each do |c|
+
+      name = c.css("h3.slide-title").text.split(/[:]/).delete_at(0).split.last #(doesn't work ask Corinna)  
+
+      binding.pry
+      tourists = c.css("h3.slide-title").text.split(" ").last(2).join(" ")
+      info = c.css("p").text.split("?")
+      info = c.css("p span")
+      # why_qns.each {|each_qn| "#{each_qn}?"}
+      Country.new(name, tourists, info)
+
     end
    end
+
+
 end
 
-scraper = Scraper.new
-scraper.get_page
-scraper.scrape_main_page
-scraper.create_countries
+
+Scraper.get_page
+Scraper.scrape_main_page
+Scraper.create_countries
+Scraper.scrape_country_info
