60d1e70850ba3a0d91e29eb5159984af63df77f7
vanita eckert <vanita112003@gmail.com>
2017-01-25 21:17:45 +0000

    refactor

--- lib/least_travelled.rb
+++ lib/least_travelled.rb
@@ -3,7 +3,6 @@ require 'pry'
 require 'open-uri'
 require 'nokogiri'
 
-
 require_relative '../lib/least_travelled/country'
 require_relative '../lib/least_travelled/scraper'
 require_relative '../lib/least_travelled/CLI_interface'
